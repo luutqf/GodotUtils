c63b3fb0e5f6ec8f73487fe4f9ff5d83d486f986
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-12-08 20:02:59 -0600

    Update State.cs

--- State.cs
+++ State.cs
@@ -5,8 +5,7 @@ namespace RedotUtils;
 public class State(string name = "")
 {
     public Action Enter { get; set; } = () => { };
-    public Action<float> Update { get; set; } = delta => { };
-    public Action Transitions { get; set; } = () => { };
+    public Action<double> Update { get; set; } = delta => { };
     public Action Exit { get; set; } = () => { };
 
     private string _name = name;
