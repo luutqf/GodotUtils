3ade50f165bcc1a4acecf9e076195343a15a0916
Valk <sebastianbelle074@protonmail.com>
2025-04-27 19:49:35 -0400

    Create Debug.cs

---
+++ Debug.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GodotUtils;
+
+internal static class Debug
+{
+    internal static void Assert(bool condition, string message)
+    {
+#if DEBUG
+        if (condition)
+            return;
+
+        throw new Exception(message);
+#endif
+    }
+}
