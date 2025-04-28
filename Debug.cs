e2095a41d71f3c762a14828b4fda49dabae01de3
Valk <sebastianbelle074@protonmail.com>
2025-04-27 21:34:32 -0400

    Update Debug

--- Debug.cs
+++ Debug.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace GodotUtils;
 
+// The whole reason I made this was to try and skip the first two frames of the stack trace but because Godot is doing something
+// weird, it does not work and this code is useless but I will leave it here in case anyone else figures it out.
 internal static class Debug
 {
+    [StackTraceHidden]
     internal static void Assert(bool condition, string message)
     {
 #if DEBUG
         if (condition)
             return;
 
-        throw new Exception(message);
+        throw new AssertException(message);
 #endif
+    }
+
+    private class AssertException(string message) : Exception(message)
+    {
+        public override string StackTrace => new StackTrace(skipFrames: 2, true).ToString();
     }
 }
