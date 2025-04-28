761e14649e0103d9c507c3e4a6a8fc095ed676e4
Valk <sebastianbelle074@protonmail.com>
2025-04-27 21:49:04 -0400

    Update Debug.cs

--- Debug.cs
+++ Debug.cs
@@ -7,7 +7,6 @@ namespace GodotUtils;
 // weird, it does not work and this code is useless but I will leave it here in case anyone else figures it out.
 internal static class Debug
 {
-    [StackTraceHidden]
     internal static void Assert(bool condition, string message)
     {
 #if DEBUG
