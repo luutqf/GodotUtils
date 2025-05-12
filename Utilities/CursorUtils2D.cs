618e17da16dcae6514cd88a283b28daac456b21c
Valk <sebastianbelle074@protonmail.com>
2025-05-12 14:42:40 -0400

    Update CursorUtils2D.cs

--- Utilities/CursorUtils2D.cs
+++ Utilities/CursorUtils2D.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GodotUtils;
 
@@ -8,60 +9,27 @@ namespace GodotUtils;
 /// </summary>
 public static class CursorUtils2D
 {
-    /// <summary>
-    /// Retrieves the <see cref="Area2D"/> node under the cursor's current position.
-    /// </summary>
-    /// <param name="node">The <see cref="Node2D"/> node from which to start the query.</param>
-    /// <returns>The <see cref="Area2D"/> node under the cursor, or null if none is found.</returns>
-    public static Area2D GetAreaUnderCursor(Node2D node)
+    public static List<Area2D> GetAreasUnderCursor(Node2D node, int maxResults = 1)
     {
-        return (Area2D)GetPhysicsNodeAtPosition(node, node.GetGlobalMousePosition(),
-            true, false, false);
+        return GetPhysicsNodesAtPosition(node, node.GetGlobalMousePosition(), true, false, false, maxResults).Select(x => x as Area2D).ToList();
     }
-
-    /// <summary>
-    /// Retrieves the <see cref="PhysicsBody2D"/> node under the cursor's current position.
-    /// </summary>
-    /// <param name="node">The <see cref="Node2D"/> node from which to start the query.</param>
-    /// <returns>The <see cref="PhysicsBody2D"/> node under the cursor, or null if none is found.</returns>
-    public static PhysicsBody2D GetBodyUnderCursor(Node2D node)
+    
+    public static List<PhysicsBody2D> GetBodiesUnderCursor(Node2D node, int maxResults = 1)
     {
-        return (PhysicsBody2D)GetPhysicsNodeAtPosition(node, node.GetGlobalMousePosition(),
-            false, true, false);
+        return GetPhysicsNodesAtPosition(node, node.GetGlobalMousePosition(), false, true, false, maxResults).Select(x => x as PhysicsBody2D).ToList();
     }
-
-    /// <summary>
-    /// Retrieves the <see cref="Area2D"/> node under the specified <see cref="Node2D"/> node's global position.
-    /// </summary>
-    /// <param name="node">The <see cref="Node2D"/> node from which to start the query.</param>
-    /// <returns>The <see cref="Area2D"/> node under the specified position, or null if none is found.</returns>
-    public static Area2D GetAreaUnder(Node2D node)
+    
+    public static List<Area2D> GetAreasUnder(Node2D node, int maxResults = 1)
     {
-        return (Area2D)GetPhysicsNodeAtPosition(node, node.GlobalPosition,
-            true, false, true);
+        return GetPhysicsNodesAtPosition(node, node.GlobalPosition, true, false, true, maxResults).Select(x => x as Area2D).ToList();
     }
-
-    /// <summary>
-    /// Retrieves the <see cref="PhysicsBody2D"/> node under the specified <see cref="Node2D"/> node's global position.
-    /// </summary>
-    /// <param name="node">The <see cref="Node2D"/> node from which to start the query.</param>
-    /// <returns>The <see cref="PhysicsBody2D"/> node under the specified position, or null if none is found.</returns>
-    public static PhysicsBody2D GetBodyUnder(Node2D node)
+    
+    public static List<PhysicsBody2D> GetBodiesUnder(Node2D node, int maxResults = 1)
     {
-        return (PhysicsBody2D)GetPhysicsNodeAtPosition(node, node.GlobalPosition,
-            false, true, true);
+        return GetPhysicsNodesAtPosition(node, node.GlobalPosition, false, true, true, maxResults).Select(x => x as PhysicsBody2D).ToList();
     }
-
-    /// <summary>
-    /// Retrieves a physics node (either <see cref="Area2D"/> or <see cref="PhysicsBody2D"/>) at the specified position.
-    /// </summary>
-    /// <param name="node">The <see cref="Node2D"/> node from which to start the query.</param>
-    /// <param name="position">The position in the world to query.</param>
-    /// <param name="collideWithAreas">Whether to collide with <see cref="Area2D"/> nodes.</param>
-    /// <param name="collideWithBodies">Whether to collide with <see cref="PhysicsBody2D"/> nodes.</param>
-    /// <param name="excludeSelf">Whether to exclude the node itself and its children from the query.</param>
-    /// <returns>The physics node at the specified position, or null if none is found.</returns>
-    private static Node GetPhysicsNodeAtPosition(Node2D node, Vector2 position, bool collideWithAreas, bool collideWithBodies, bool excludeSelf = false)
+    
+    private static List<Node> GetPhysicsNodesAtPosition(Node2D node, Vector2 position, bool collideWithAreas, bool collideWithBodies, bool excludeSelf = false, int maxResults = 1)
     {
         // Create a shape query parameters object
         PhysicsPointQueryParameters2D queryParams = new();
@@ -69,6 +37,7 @@ public static class CursorUtils2D
         queryParams.CollideWithAreas = collideWithAreas;
         queryParams.CollideWithBodies = collideWithBodies;
 
+        // Exclude the node itself and its children from the query
         if (excludeSelf)
         {
             List<Rid> rids = [];
@@ -85,20 +54,20 @@ public static class CursorUtils2D
         }
 
         // Perform the query
-        PhysicsDirectSpaceState2D spaceState =
-            PhysicsServer2D.SpaceGetDirectState(node.GetWorld2D().GetSpace());
+        PhysicsDirectSpaceState2D spaceState = PhysicsServer2D.SpaceGetDirectState(node.GetWorld2D().GetSpace());
+        
+        Godot.Collections.Array<Godot.Collections.Dictionary> results = spaceState.IntersectPoint(queryParams, maxResults);
 
-        Godot.Collections.Array<Godot.Collections.Dictionary> results =
-            spaceState.IntersectPoint(queryParams, 1);
+        List<Node> nodes = [];
 
         foreach (Godot.Collections.Dictionary result in results)
         {
             if (result != null && result.ContainsKey("collider"))
             {
-                return result["collider"].As<Node>();
+                nodes.Add(result["collider"].As<Node>());
             }
         }
 
-        return null;
+        return nodes;
     }
 }
