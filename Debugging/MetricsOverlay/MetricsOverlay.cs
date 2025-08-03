e420c6737cb61db4c9f8126db8d843b687212007
Valk <sebastianbelle074@protonmail.com>
2025-08-03 02:00:53 -0400

    Clean up MetricsOverlay.cs

--- Debugging/MetricsOverlay/MetricsOverlay.cs
+++ Debugging/MetricsOverlay/MetricsOverlay.cs
@@ -2,60 +2,67 @@ using Godot;
 using ImGuiNET;
 using System;
 using System.Collections.Generic;
-using System.Text.Json;
 using Monitor = Godot.Performance.Monitor;
 using Vector2 = System.Numerics.Vector2;
 
 namespace GodotUtils.Debugging;
 
-[SceneTree]
 public partial class MetricsOverlay : Component
 {
-    private const int BYTES_IN_MEGABYTE = 1048576;
-    private const int BYTES_IN_KILOBYTE = 1024;
-    private const int MAX_FPS_BUFFER    = 100;
-    private const int WINDOW_WIDTH      = 220;
+    private const int BytesInMegabyte     = 1048576;
+    private const int BytesInKilobyte     = 1024;
+    private const int MaxFpsBuffer        = 100;
+    private const int WindowWidth         = 220;
+    private const int FpsGraphWidthMargin = 15;
+    private const int FpsGraphHeight      = 30;
 
-    private float[] _fpsBuffer = new float[MAX_FPS_BUFFER];
-    private float   _cachedFPS;
+    private const string ImGuiWindowName = "Metrics Overlay";
+    private const string LabelMetrics    = "Metrics";
+    private const string LabelVariables  = "Variables";
+    private const string LabelFpsGraph   = "##FPSGraph"; // The ## hides the text
+
+    private float[] _fpsBuffer = new float[MaxFpsBuffer];
+    private float   _cachedFps;
     private int     _fpsIndex;
-    private bool    _enableFPSGraph;
 
     private bool _visible;
 
-    private Dictionary<string, (bool enabled, Func<string> valueProvider)> _metrics;
-    private static Dictionary<string, Func<object>> _trackingVariables = [];
-
-    private static double Retrieve(Monitor monitor) => Performance.GetMonitor(monitor);
-
-    private const string SETTINGS_FILE = "user://metrics_settings.json";
+    private static Dictionary<string, Func<object>> _trackingVariables = []; // Should this really be static?
+    private Dictionary<string, Func<string>> _currentMetrics = [];
 
     public override void Ready()
     {
         RegisterProcess();
 
-        _metrics = new()
+        Dictionary<string, (bool Enabled, Func<string> ValueProvider)> metrics = new()
         {
-            { "FPS",                    (true,  () => $"{_cachedFPS}") },
-            { "Process",                (true,  () => $"{Retrieve(Monitor.TimeProcess) * 1000:0.00} ms") },
+            { "FPS",                    (true,  () => $"{_cachedFps}") },
+            { "Process",                (false, () => $"{Retrieve(Monitor.TimeProcess) * 1000:0.00} ms") },
             { "Physics Process",        (false, () => $"{Retrieve(Monitor.TimePhysicsProcess) * 1000:0.00} ms") },
             { "Navigation Process",     (false, () => $"{Retrieve(Monitor.TimeNavigationProcess) * 1000:0.00} ms") },
-            { "Static Memory",          (true,  () => $"{Retrieve(Monitor.MemoryStatic) / BYTES_IN_MEGABYTE:0.0} MiB") },
-            { "Static Memory Max",      (false, () => $"{Retrieve(Monitor.MemoryStaticMax) / BYTES_IN_MEGABYTE:0.0} MiB") },
-            { "Message Buffer Max",     (false, () => $"{Retrieve(Monitor.MemoryMessageBufferMax) / BYTES_IN_KILOBYTE:0.0} KiB") },
-            { "Object Count",           (true,  () => $"{Retrieve(Monitor.ObjectCount)}") },
-            { "Resource Count",         (true,  () => $"{Retrieve(Monitor.ObjectResourceCount)}") },
+            { "Static Memory",          (true,  () => $"{Retrieve(Monitor.MemoryStatic) / BytesInMegabyte:0.0} MiB") },
+            { "Static Memory Max",      (false, () => $"{Retrieve(Monitor.MemoryStaticMax) / BytesInMegabyte:0.0} MiB") },
+            { "Video Memory",           (true,  () => $"{Retrieve(Monitor.RenderVideoMemUsed) / BytesInMegabyte:0.0} MiB") },
+            { "Texture Memory",         (false, () => $"{Retrieve(Monitor.RenderTextureMemUsed) / BytesInMegabyte:0.0} MiB") },
+            { "Buffer Memory",          (false, () => $"{Retrieve(Monitor.RenderBufferMemUsed) / BytesInMegabyte:0.0} MiB") },
+            { "Message Buffer Max",     (false, () => $"{Retrieve(Monitor.MemoryMessageBufferMax) / BytesInKilobyte:0.0} KiB") },
+            { "Resource Count",         (false, () => $"{Retrieve(Monitor.ObjectResourceCount)}") },
             { "Node Count",             (true,  () => $"{Retrieve(Monitor.ObjectNodeCount)}") },
             { "Orphan Node Count",      (true,  () => $"{Retrieve(Monitor.ObjectOrphanNodeCount)}") },
+            { "Object Count",           (true,  () => $"{Retrieve(Monitor.ObjectCount)}") },
             { "Total Objects Drawn",    (false, () => $"{Retrieve(Monitor.RenderTotalObjectsInFrame)}") },
             { "Total Primitives Drawn", (false, () => $"{Retrieve(Monitor.RenderTotalPrimitivesInFrame)}") },
             { "Total Draw Calls",       (false, () => $"{Retrieve(Monitor.RenderTotalDrawCallsInFrame)}") },
-            { "Video Memory",           (true,  () => $"{Retrieve(Monitor.RenderVideoMemUsed) / BYTES_IN_MEGABYTE:0.0} MiB") },
-            { "Texture Memory",         (false, () => $"{Retrieve(Monitor.RenderTextureMemUsed) / BYTES_IN_MEGABYTE:0.0} MiB") },
-            { "Buffer Memory",          (false, () => $"{Retrieve(Monitor.RenderBufferMemUsed) / BYTES_IN_MEGABYTE:0.0} MiB") }
         };
 
-        LoadSettings();
+        // Perfect example of where var is useful...
+        foreach (var metric in metrics)
+        {
+            if (metric.Value.Enabled)
+            {
+                _currentMetrics.Add(metric.Key, metric.Value.ValueProvider);
+            }
+        }
     }
 
     public override void Process(double delta)
@@ -67,41 +74,8 @@ public partial class MetricsOverlay : Component
 
         if (_visible)
         {
-            RenderPerformanceMetrics();
+            RenderPerformanceMetrics(_currentMetrics, _fpsBuffer, ref _fpsIndex, ref _cachedFps);
         }
-    }
-
-    // Will be called in exported release but not in the editor
-    public override void _ExitTree()
-    {
-        SaveSettings();
-    }
-
-    private void RenderPerformanceMetrics()
-    {
-        _cachedFPS = (float)Retrieve(Monitor.TimeFps);
-        UpdateFpsBuffer();
-
-        SetupImGuiWindow();
-
-        RenderSettings();
-        RenderMetrics();
-        RenderUserDefinedVariables();
-
-        ImGui.End();
-    }
-
-    private static void SetupImGuiWindow()
-    {
-        // Print the window scale
-        Godot.Vector2 size = DisplayServer.WindowGetSize();
-        Vector2 screenSize = new(size.X, size.Y);
-        Vector2 windowSize = new(WINDOW_WIDTH, 0);
-        Vector2 topRight = new Vector2(screenSize.X, 0) - new Vector2(windowSize.X, -ImGui.GetFrameHeight());
-
-        ImGui.SetNextWindowPos(topRight, ImGuiCond.Always);
-        ImGui.SetNextWindowSize(windowSize, ImGuiCond.Always);
-        ImGui.Begin("Metrics Overlay", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoTitleBar);
     }
 
     public static void StartTracking(string key, Func<object> function)
@@ -114,54 +88,56 @@ public partial class MetricsOverlay : Component
         _trackingVariables.Remove(key);
     }
 
-    private void RenderSettings()
+    private static void RenderPerformanceMetrics(Dictionary<string, Func<string>> metrics, float[] fpsBuffer, ref int fpsIndex, ref float cachedFps)
     {
-        if (!ImGui.CollapsingHeader("Settings"))
-            return;
+        UpdateFpsBuffer(ref cachedFps, fpsBuffer, ref fpsIndex);
 
-        ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, Vector2.One * 2);
-        ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, Vector2.One * 2);
+        SetupImGuiWindow();
 
-        ImGui.Checkbox("Enable FPS Graph", ref _enableFPSGraph);
+        RenderMetrics(metrics, fpsBuffer, fpsIndex);
+        RenderUserDefinedVariables();
 
-        foreach (string key in _metrics.Keys)
-        {
-            bool enabled = _metrics[key].enabled;
-
-            if (ImGui.Checkbox($"Log {key}", ref enabled))
-            {
-                _metrics[key] = (enabled, _metrics[key].valueProvider);
-            }
-        }
-
-        ImGui.PopStyleVar(2);
+        ImGui.End();
     }
 
-    private void RenderMetrics()
+    private static void SetupImGuiWindow()
     {
-        if (!ImGui.CollapsingHeader("Metrics", ImGuiTreeNodeFlags.DefaultOpen))
+        // Print the window scale
+        Godot.Vector2 size = DisplayServer.WindowGetSize();
+        Vector2 screenSize = new(size.X, size.Y);
+        Vector2 windowSize = new(WindowWidth, 0);
+        Vector2 topRight = new Vector2(screenSize.X, 0) - new Vector2(windowSize.X, -ImGui.GetFrameHeight());
+
+        ImGui.SetNextWindowPos(topRight, ImGuiCond.Always);
+        ImGui.SetNextWindowSize(windowSize, ImGuiCond.Always);
+
+        ImGuiWindowFlags flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoTitleBar;
+
+        ImGui.Begin(ImGuiWindowName, flags);
+    }
+
+    private static void RenderMetrics(Dictionary<string, Func<string>> metrics, float[] fpsBuffer, int fpsIndex)
+    {
+        if (!ImGui.CollapsingHeader(LabelMetrics, ImGuiTreeNodeFlags.DefaultOpen))
             return;
 
-        foreach ((string key, (bool enabled, Func<string> valueProvider)) in _metrics)
+        foreach ((string key, Func<string> valueProvider) in metrics)
         {
-            if (enabled)
-            {
-                ImGui.Text($"{key}: {valueProvider()}");
+            ImGui.Text($"{key}: {valueProvider()}");
 
-                if (key == "FPS" && _enableFPSGraph)
-                {
-                    RenderFpsGraph();
-                }
+            if (key == "FPS")
+            {
+                RenderFpsGraph(fpsBuffer, fpsIndex);
             }
         }
     }
 
-    private void RenderUserDefinedVariables()
+    private static void RenderUserDefinedVariables()
     {
-        if (_trackingVariables.Count == 0 || !ImGui.CollapsingHeader("User Defined", ImGuiTreeNodeFlags.DefaultOpen))
+        if (_trackingVariables.Count == 0 || !ImGui.CollapsingHeader(LabelVariables, ImGuiTreeNodeFlags.DefaultOpen))
             return;
 
-        foreach (var kvp in _trackingVariables)
+        foreach (KeyValuePair<string, Func<object>> kvp in _trackingVariables)
         {
             string name = kvp.Key;
             string value = kvp.Value().ToString();
@@ -170,65 +146,25 @@ public partial class MetricsOverlay : Component
         }
     }
 
-    private void RenderFpsGraph()
+    private static void UpdateFpsBuffer(ref float cachedFps, float[] fpsBuffer, ref int fpsIndex)
+    {
+        cachedFps = (float)Retrieve(Monitor.TimeFps);
+        fpsBuffer[fpsIndex] = cachedFps;
+        fpsIndex = (fpsIndex + 1) % fpsBuffer.Length;
+    }
+
+    private static void RenderFpsGraph(float[] fpsBuffer, int fpsIndex)
     {
         ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, Vector2.Zero);
 
-        ImGui.PlotLines("##FPSGraph", ref _fpsBuffer[0], _fpsBuffer.Length, _fpsIndex,
+        ImGui.PlotLines(LabelFpsGraph, ref fpsBuffer[0], fpsBuffer.Length, fpsIndex,
             overlay_text: null,
             scale_min: 0,
             scale_max: DisplayServer.ScreenGetRefreshRate(),
-            graph_size: new Vector2(WINDOW_WIDTH - 15, 30));
+            graph_size: new Vector2(WindowWidth - FpsGraphWidthMargin, FpsGraphHeight));
 
         ImGui.PopStyleVar();
     }
 
-    private void UpdateFpsBuffer()
-    {
-        _fpsBuffer[_fpsIndex] = _cachedFPS;
-        _fpsIndex = (_fpsIndex + 1) % _fpsBuffer.Length;
-    }
-
-    private void SaveSettings()
-    {
-        Dictionary<string, bool> settings = [];
-
-        foreach (string key in _metrics.Keys)
-        {
-            settings[key] = _metrics[key].enabled;
-        }
-
-        string json = JsonSerializer.Serialize(settings, new JsonSerializerOptions
-        {
-            WriteIndented = true
-        });
-
-        FileAccess file = FileAccess.Open(SETTINGS_FILE, FileAccess.ModeFlags.Write);
-
-        file.StoreString(json);
-        file.Close();
-    }
-
-    private void LoadSettings()
-    {
-        if (!FileAccess.FileExists(SETTINGS_FILE))
-            return;
-
-        FileAccess file = FileAccess.Open(SETTINGS_FILE, FileAccess.ModeFlags.Read);
-        string json = file.GetAsText();
-        file.Close();
-
-        Dictionary<string, bool> settings = JsonSerializer.Deserialize<Dictionary<string, bool>>(json);
-
-        if (settings == null)
-            return;
-
-        foreach (string key in settings.Keys)
-        {
-            if (_metrics.TryGetValue(key, out (bool enabled, Func<string> valueProvider) value))
-            {
-                _metrics[key] = (settings[key], value.valueProvider);
-            }
-        }
-    }
+    private static double Retrieve(Monitor monitor) => Performance.GetMonitor(monitor);
 }
