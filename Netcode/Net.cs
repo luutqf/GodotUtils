d48543b7fb5d0ddee92e52777b38f5f54e2b858f
Valk <sebastianbelle074@protonmail.com>
2025-08-09 11:43:35 -0500

    Move Net and NetControlPanelLow to GodotUtils

---
+++ Netcode/Net.cs
@@ -0,0 +1,137 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+using GodotUtils;
+using GodotUtils.Netcode;
+using GodotUtils.Netcode.Client;
+using GodotUtils.Netcode.Server;
+
+namespace __TEMPLATE__.Netcode;
+
+public class Net
+{
+    public event Action<ENetServer> ServerCreated;
+    public event Action<ENetClient> ClientCreated;
+
+    public static int HeartbeatPosition { get; } = 20;
+
+    public ENetServer Server { get; private set; }
+    public ENetClient Client { get; private set; }
+
+    private const int ShutdownPollIntervalMs = 1;
+
+    private IGameServerFactory _serverFactory;
+    private IGameClientFactory _clientFactory;
+
+    public Net(Node node, IGameServerFactory serverFactory, IGameClientFactory clientFactory)
+    {
+        Global.Instance.PreQuit += StopThreads;
+
+        _serverFactory = serverFactory;
+        _clientFactory = clientFactory;
+
+        Server = serverFactory.CreateServer();
+        Client = clientFactory.CreateClient();
+    }
+
+    ~Net()
+    {
+        GD.Print("Net deconstructor");
+    }
+
+    public void StopServer()
+    {
+        Server.Stop();
+    }
+
+    public void StartServer()
+    {
+        if (Server.IsRunning)
+        {
+            Server.Log("Server is running already");
+            return;
+        }
+
+        Server = _serverFactory.CreateServer();
+        ServerCreated?.Invoke(Server);
+        Server.Start(25565, 100, new ENetOptions
+        {
+            PrintPacketByteSize = false,
+            PrintPacketData = false,
+            PrintPacketReceived = false,
+            PrintPacketSent = false
+        });
+
+        Services.Get<UI.PopupMenu>().MainMenuBtnPressed += () =>
+        {
+            Server.Stop();
+        };
+    }
+
+    public void StartClient(string ip, ushort port)
+    {
+        if (Client.IsRunning)
+        {
+            Client.Log("Client is running already");
+            return;
+        }
+
+        Client = _clientFactory.CreateClient();
+
+        ClientCreated?.Invoke(Client);
+
+        Client.Connect(ip, port, new ENetOptions
+        {
+            PrintPacketByteSize = false,
+            PrintPacketData = false,
+            PrintPacketReceived = false,
+            PrintPacketSent = false
+        });
+    }
+
+    public void StopClient()
+    {
+        if (!Client.IsRunning)
+        {
+            Client.Log("Client was stopped already");
+            return;
+        }
+
+        Client.Stop();
+    }
+
+    private async Task StopThreads()
+    {
+        // Stop the server and client
+        if (ENetLow.ENetInitialized)
+        {
+            if (Server.IsRunning)
+            {
+                Server.Stop();
+
+                while (Server.IsRunning)
+                {
+                    await Task.Delay(ShutdownPollIntervalMs);
+                }
+            }
+
+            if (Client.IsRunning)
+            {
+                Client.Stop();
+
+                while (Client.IsRunning)
+                {
+                    await Task.Delay(ShutdownPollIntervalMs);
+                }
+            }
+
+            ENet.Library.Deinitialize();
+        }
+
+        // Wait for the logger to finish enqueing the remaining logs
+        while (Logger.StillWorking())
+        {
+            await Task.Delay(ShutdownPollIntervalMs);
+        }
+    }
+}
---
+++ Netcode/NetControlPanelLow.cs
@@ -0,0 +1,85 @@
+using Godot;
+using GodotUtils;
+using GodotUtils.Netcode;
+
+namespace __TEMPLATE__.Netcode;
+
+public abstract partial class NetControlPanelLow : Control
+{
+    public Net Net { get; private set; }
+
+    private string _ip = "127.0.0.1";
+    private ushort _port = 25565;
+    private string _username = "";
+
+    public abstract IGameServerFactory GameServerFactory();
+    public abstract IGameClientFactory GameClientFactory();
+    public abstract void StartClientButtonPressed(string username);
+
+    public override void _Ready()
+    {
+        Net = new Net(this, GameServerFactory(), GameClientFactory());
+
+        SetupButtons();
+        SetupInputFields();
+        SetupClientEvents();
+    }
+
+    private void SetupButtons()
+    {
+        GetNode<Button>("%Start Server").Pressed += Net.StartServer;
+        GetNode<Button>("%Stop Server").Pressed += Net.StopServer;
+        GetNode<Button>("%Start Client").Pressed += () =>
+        {
+            StartClientButtonPressed(_username);
+            Net.StartClient(_ip, _port);
+        };
+        GetNode<Button>("%Stop Client").Pressed += Net.StopClient;
+    }
+
+    private void SetupInputFields()
+    {
+        GetNode<LineEdit>("%IP").TextChanged += text =>
+        {
+            string[] parts = text.Split(":");
+            _ip = parts[0];
+            if (parts.Length > 1 && ushort.TryParse(parts[1], out ushort port))
+            {
+                _port = port;
+            }
+        };
+
+        GetNode<LineEdit>("%Username").TextChanged += text =>
+        {
+            _username = text.IsAlphaNumeric() ? text : _username;
+        };
+    }
+
+    private void SetupClientEvents()
+    {
+        Net.ClientCreated += client =>
+        {
+            client.Connected += () =>
+            {
+                if (!Net.Server.IsRunning)
+                {
+                    GetNode<Button>("%Start Server").Disabled = true;
+                    GetNode<Button>("%Stop Server").Disabled = true;
+                }
+
+                GetTree().UnfocusCurrentControl();
+            };
+
+            client.Disconnected += _ =>
+            {
+                GetNode<Button>("%Start Server").Disabled = false;
+                GetNode<Button>("%Stop Server").Disabled = false;
+            };
+        };
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Net.Client?.HandlePackets();
+    }
+}
