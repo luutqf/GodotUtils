a8e4d8c4bba98d325ae25e0f737b28e4d6894474
Valk <sebastianbelle074@protonmail.com>
2025-08-09 11:51:28 -0500

    Cleanup NetControlPanelLow.cs

--- Netcode/NetControlPanelLow.cs
+++ Netcode/NetControlPanelLow.cs
@@ -1,4 +1,5 @@
 using Godot;
+using GodotUtils.Netcode.Client;
 
 namespace GodotUtils.Netcode;
 
@@ -6,13 +7,12 @@ public abstract partial class NetControlPanelLow : Control
 {
     public Net Net { get; private set; }
 
-    private string _ip = "127.0.0.1";
-    private ushort _port = 25565;
-    private string _username = "";
+    private const string LocalIp = "127.0.0.1";
+    private const ushort DefaultPort = 25565;
 
-    public abstract IGameServerFactory GameServerFactory();
-    public abstract IGameClientFactory GameClientFactory();
-    public abstract void StartClientButtonPressed(string username);
+    private string _ip = LocalIp;
+    private ushort _port = DefaultPort;
+    private string _username = "";
 
     public override void _Ready()
     {
@@ -23,61 +23,77 @@ public abstract partial class NetControlPanelLow : Control
         SetupClientEvents();
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        Net.Client?.HandlePackets();
+    }
+
+    public abstract IGameServerFactory GameServerFactory();
+    public abstract IGameClientFactory GameClientFactory();
+    public abstract void StartClientButtonPressed(string username);
+
     private void SetupButtons()
     {
         GetNode<Button>("%Start Server").Pressed += Net.StartServer;
         GetNode<Button>("%Stop Server").Pressed += Net.StopServer;
-        GetNode<Button>("%Start Client").Pressed += () =>
-        {
-            StartClientButtonPressed(_username);
-            Net.StartClient(_ip, _port);
-        };
+        GetNode<Button>("%Start Client").Pressed += OnStartClientBtnPressed;
         GetNode<Button>("%Stop Client").Pressed += Net.StopClient;
+    }
+
+    private void OnStartClientBtnPressed()
+    {
+        StartClientButtonPressed(_username);
+        Net.StartClient(_ip, _port);
     }
 
     private void SetupInputFields()
     {
-        GetNode<LineEdit>("%IP").TextChanged += text =>
-        {
-            string[] parts = text.Split(":");
-            _ip = parts[0];
-            if (parts.Length > 1 && ushort.TryParse(parts[1], out ushort port))
-            {
-                _port = port;
-            }
-        };
+        GetNode<LineEdit>("%IP").TextChanged += OnIpChanged;
+        GetNode<LineEdit>("%Username").TextChanged += OnUsernameChanged;
+    }
 
-        GetNode<LineEdit>("%Username").TextChanged += text =>
+    private void OnIpChanged(string text)
+    {
+        string[] parts = text.Split(":");
+
+        _ip = parts[0];
+
+        if (parts.Length > 1 && ushort.TryParse(parts[1], out ushort port))
         {
-            _username = text.IsAlphaNumeric() ? text : _username;
-        };
+            _port = port;
+        }
+    }
+
+    private void OnUsernameChanged(string text)
+    {
+        _username = text.IsAlphaNumeric() ? text : _username;
     }
 
     private void SetupClientEvents()
     {
-        Net.ClientCreated += client =>
-        {
-            client.Connected += () =>
-            {
-                if (!Net.Server.IsRunning)
-                {
-                    GetNode<Button>("%Start Server").Disabled = true;
-                    GetNode<Button>("%Stop Server").Disabled = true;
-                }
-
-                GetTree().UnfocusCurrentControl();
-            };
-
-            client.Disconnected += _ =>
-            {
-                GetNode<Button>("%Start Server").Disabled = false;
-                GetNode<Button>("%Stop Server").Disabled = false;
-            };
-        };
+        Net.ClientCreated += OnClientCreated;
     }
 
-    public override void _PhysicsProcess(double delta)
+    private void OnClientCreated(ENetClient client)
     {
-        Net.Client?.HandlePackets();
+        client.Connected += OnClientConnected;
+        client.Disconnected += OnClientDisconnected;
+    }
+
+    private void OnClientConnected()
+    {
+        if (!Net.Server.IsRunning)
+        {
+            GetNode<Button>("%Start Server").Disabled = true;
+            GetNode<Button>("%Stop Server").Disabled = true;
+        }
+
+        GetTree().UnfocusCurrentControl();
+    }
+
+    private void OnClientDisconnected(DisconnectOpcode opcode)
+    {
+        GetNode<Button>("%Start Server").Disabled = false;
+        GetNode<Button>("%Stop Server").Disabled = false;
     }
 }
